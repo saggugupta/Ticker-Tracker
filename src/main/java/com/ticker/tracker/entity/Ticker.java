7eecaf3fcd5ca4c86c465be86c8713838ffb257c
Sagar gupta <sagargupta0761@gmail.com>
2022-04-17 22:14:02 +0530

    update to mysql
    update to mysql

--- src/main/java/com/ticker/tracker/entity/Ticker.java
+++ src/main/java/com/ticker/tracker/entity/Ticker.java
@@ -4,16 +4,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(uniqueConstraints={
+        @UniqueConstraint(columnNames = {"symbol","token" })
+})
 public class Ticker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
