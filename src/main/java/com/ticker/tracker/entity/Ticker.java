6f1e1cc25e7efd38a2770ae36f34fadf73a28ca9
Sagar gupta <sagargupta0761@gmail.com>
2022-05-19 12:41:38 +0530

    Update Ticker.java

--- src/main/java/com/ticker/tracker/entity/Ticker.java
+++ src/main/java/com/ticker/tracker/entity/Ticker.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.Objects;
